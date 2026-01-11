867d4d57712a69c7fecb09fa92d673db58185f1e
daanniill <daniil.malakhov12@gmail.com>
2026-01-10 18:55:27 -0800

    configure webpack

--- webpack.common.js
+++ webpack.common.js
@@ -4,21 +4,20 @@ const webpack = require("webpack");
 require("dotenv").config();
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/REPO_NAME/",
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
-
     new webpack.DefinePlugin({
-      "process.env.API_KEY": JSON.stringify(
-        process.env.API_KEY
-      ),
+      "process.env.API_KEY": JSON.stringify(process.env.API_KEY),
     }),
   ],
   module: {
